720bd5b9e7c57e57070438c95196b3baadc81b80
Andreas Dausenau <d4us1@MacBook-Pro-von-Andreas.fritz.box>
2023-01-27 21:56:22 +0100

    Updated dependencies versions. Updated version. Replaced tslint package with eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+    "ignorePatterns": ["build/**/*"],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    plugins: ['prefer-arrow'],
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    extends: [
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'plugin:prettier/recommended'
+    ],
+    rules: {
+        "react/jsx-uses-react": "off",
+        "react/react-in-jsx-scope": "off",
+        'prettier/prettier': 'warn',
+        '@typescript-eslint/explicit-module-boundary-types': 'off', // explicit return type on react functions should be disabled
+        '@typescript-eslint/ban-ts-comment': [
+            'error',
+            {
+                'ts-expect-error': 'allow-with-description',
+                'ts-ignore': 'allow-with-description',
+                'ts-nocheck': 'allow-with-description',
+                'ts-check': 'allow-with-description'
+            }
+        ],
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/no-var-requires': 'warn',
+        'prefer-arrow/prefer-arrow-functions': [
+            'error',
+            {
+                disallowPrototype: true,
+                singleReturnOnly: false,
+                classPropertiesAllowed: false,
+            }
+        ],
+        "padding-line-between-statements": [
+            "error",
+            { "blankLine": "always", "prev": "multiline-block-like", "next": "*" }
+        ]
+    }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    semi: true,
+    trailingComma: 'none',
+    singleQuote: true,
+    bracketSpacing: true,
+    printWidth: 120,
+    tabWidth: 4
+};
